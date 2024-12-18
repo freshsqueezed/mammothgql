6161cd3c1426d10fe6a49637b29c7223efbaa81b
matthew-gordon <m.gordon5525@gmail.com>
2024-12-17 18:16:22 -0700

    Refactor graphiql, and request validation

--- src/mammoth.ts
+++ src/mammoth.ts
@@ -1,55 +1,120 @@
 import { NextFunction, Request, Response } from 'express';
 import {
-  ExecutionResult,
-  GraphQLSchema,
+  execute,
+  getOperationAST,
+  GraphQLError,
   parse,
   Source,
   specifiedRules,
   validate,
+  validateSchema,
 } from 'graphql';
-import { InternalServerError, ValidationError } from './errors';
-import { formatExecutionErrors, handleValidationErrors } from './utils';
-import { executeQuery } from './query';
+import type {
+  GraphQLSchema,
+  DocumentNode,
+  FormattedExecutionResult,
+  GraphQLFormattedError,
+} from 'graphql';
+import { handleValidationErrors } from './utils';
 import { customLandingHtml, disabledLandingPage, graphiqlHtml } from './html';
 
 export interface MammothOptions<TContext> {
   schema: GraphQLSchema;
   context: ({ req, res }: { req: Request; res: Response }) => TContext;
+  pretty?: boolean;
   graphiql?: boolean;
+  validationRules: any[];
 }
 
-export function mammothGraphql<TContext>({
-  schema,
-  context,
-  graphiql = false,
-}: MammothOptions<TContext>) {
+export function mammothGraphql<TContext>(options: MammothOptions<TContext>) {
+  const schema = options.schema;
+  const pretty = options.pretty ?? false;
+  const validationRules = options.validationRules ?? [];
+  const showGraphiQL = options.graphiql ?? false;
+
   return async (
     req: Request,
     res: Response,
     next: NextFunction,
   ): Promise<void> => {
-    if (!graphiql) {
-      res.send(disabledLandingPage());
+    if (req.method !== 'GET' && req.method !== 'POST') {
+      res
+        .status(405)
+        .json(errorMessages(['GraphQL only supports GET and POST requests.']));
       return;
     }
 
-    if (graphiql && req.method === 'GET') {
-      if (process.env.NODE_ENV !== 'production') {
-        res.send(graphiqlHtml());
-      } else {
-        res.send(customLandingHtml());
+    const { query, variables, operationName } = req.body;
+
+    if (query == null) {
+      if (showGraphiQL && req.method === 'GET') {
+        return graphiqlHtml(req, res);
       }
+      res.status(400).json(errorMessages(['Must provide query string.']));
       return;
     }
 
-    const { query, variables } = req.body;
+    const schemaValidationErrors = validateSchema(schema);
+    if (schemaValidationErrors.length > 0) {
+      // Return 500: Internal Server Error if invalid schema.
+      res
+        .status(500)
+        .json(
+          errorMessages(
+            ['GraphQL schema validation error.'],
+            schemaValidationErrors,
+          ),
+        );
+      return;
+    }
 
-    if (!query) {
+    let documentAST: DocumentNode;
+    try {
+      documentAST = parse(new Source(query, 'GraphQL request'));
+    } catch (syntaxError: unknown) {
+      // Return 400: Bad Request if any syntax errors errors exist.
+      if (syntaxError instanceof Error) {
+        console.error(`${syntaxError.stack || syntaxError.message}`);
+        const e = new GraphQLError(syntaxError.message, {
+          originalError: syntaxError,
+        });
+        res.status(400).json(errorMessages(['GraphQL syntax error.'], [e]));
+        return;
+      }
+      throw syntaxError;
+    }
+
+    // Validate AST, reporting any errors.
+    const validationErrors = validate(schema, documentAST, [
+      ...specifiedRules,
+      ...validationRules,
+    ]);
+
+    if (validationErrors.length > 0) {
+      // Return 400: Bad Request if any validation errors exist.
       res
         .status(400)
-        .json({ errors: [new ValidationError('No query provided')] });
+        .json(errorMessages(['GraphQL validation error.'], validationErrors));
       return;
     }
+
+    if (req.method === 'GET') {
+      // Determine if this GET request will perform a non-query.
+      const operationAST = getOperationAST(documentAST, operationName);
+      if (operationAST && operationAST.operation !== 'query') {
+        // Otherwise, report a 405: Method Not Allowed error.
+        res
+          .status(405)
+          .json(
+            errorMessages([
+              `Can only perform a ${operationAST.operation} operation from a POST request.`,
+            ]),
+          );
+        return;
+      }
+    }
+
+    let result: FormattedExecutionResult;
 
     try {
       // Parse and validate the query
@@ -62,41 +127,65 @@ export function mammothGraphql<TContext>({
       }
 
       // Prepare context and execute the query
-      const contextValue: TContext = context({ req, res });
-      const executionResult: ExecutionResult = await executeQuery(
+
+      result = await execute({
         schema,
-        document,
-        contextValue,
-        variables,
-      );
-
-      // Handle errors in the execution result
-      if (executionResult.errors && executionResult.errors.length > 0) {
-        console.error('GraphQL Errors:', executionResult.errors);
-
-        const responseErrors = formatExecutionErrors(executionResult.errors);
-        res.status(200).json({ data: null, errors: responseErrors });
+        document: documentAST,
+        contextValue: options.context({ req, res }) as TContext,
+        variableValues: variables,
+        operationName: operationName,
+      });
+    } catch (contextError: unknown) {
+      if (contextError instanceof Error) {
+        console.error(`${contextError.stack || contextError.message}`);
+        const e = new GraphQLError(contextError.message, {
+          originalError: contextError,
+          nodes: documentAST,
+        });
+        // Return 400: Bad Request if any execution context errors exist.
+        res
+          .status(400)
+          .json(errorMessages(['GraphQL execution context error.'], [e]));
         return;
       }
+      throw contextError;
+    }
 
-      // Return the successful response
-      res.status(200).json({
-        data: executionResult.data,
-      });
-    } catch (error) {
-      // Handle unexpected errors
-      console.error('Server Error:', error);
+    if (!result.data) {
+      if (result.errors) {
+        res
+          .status(500)
+          .json(errorMessages([result.errors.toString()], result.errors));
+        return;
+      }
+    }
 
-      const message =
-        process.env.NODE_ENV === 'production'
-          ? 'An unexpected error occurred.'
-          : error instanceof Error
-            ? error.message
-            : String(error);
-
-      res.status(500).json({ errors: [new InternalServerError(message)] });
-    } finally {
-      next();
+    if (pretty) {
+      const payload = JSON.stringify(result, null, pretty ? 2 : 0);
+      res.status(200).send(payload);
+      return;
+    } else {
+      res.json(result);
+      return;
     }
   };
 }
+
+export const errorMessages = (
+  messages: string[],
+  graphqlErrors?: readonly GraphQLError[] | readonly GraphQLFormattedError[],
+) => {
+  if (graphqlErrors) {
+    return {
+      errors: graphqlErrors,
+    };
+  }
+
+  return {
+    errors: messages.map((message) => {
+      return {
+        message: message,
+      };
+    }),
+  };
+};
