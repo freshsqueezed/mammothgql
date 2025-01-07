6c330ec74f8e48d2803c6d6bbe4098ceb3b32a31
matthew-gordon <m.gordon5525@gmail.com>
2025-01-07 14:00:00 -0700

    fix(graphiql-request): fix broken renderer

--- src/mammoth.ts
+++ src/mammoth.ts
@@ -170,11 +170,11 @@ export function graphiqlHtml(req: Request, res: Response, cookies: string) {
   const wsUrl = `${wsProtocol}://${host}${path}`;
 
   res.send(`<!--
-*  Copyright (c) 2021 GraphQL Contributors
-*  All rights reserved.
-*
-*  This source code is licensed under the license found in the
-*  LICENSE file in the root directory of this source tree.
+ *  Copyright (c) 2021 GraphQL Contributors
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the license found in the
+ *  LICENSE file in the root directory of this source tree.
 -->
 <!doctype html>
 <html lang="en">
@@ -191,46 +191,48 @@ export function graphiqlHtml(req: Request, res: Response, cookies: string) {
       #graphiql {
         height: 100vh;
       }
-
-      .spinner {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        height: 100vh;
-        width: 100vw;
-        font-size: 24px;
-        color: #333;
-      }
     </style>
+    <!--
+      This GraphiQL example depends on Promise and fetch, which are available in
+      modern browsers, but can be "polyfilled" for older browsers.
+      GraphiQL itself depends on React DOM.
+      If you do not want to rely on a CDN, you can host these files locally or
+      include them directly in your favored resource bundler.
+    -->
     <script
       crossorigin
-      src="https://unpkg.com/react@18/umd/react.development.js"
+      src="https://unpkg.com/react@18/umd/react.production.min.js"
     ></script>
     <script
       crossorigin
-      src="https://unpkg.com/react-dom@18/umd/react-dom.development.js"
+      src="https://unpkg.com/react-dom@18/umd/react-dom.production.min.js"
     ></script>
+    <!--
+      These two files can be found in the npm module, however you may wish to
+      copy them directly into your environment, or perhaps include them in your
+      favored resource bundler.
+     -->
     <script
       src="https://unpkg.com/graphiql/graphiql.min.js"
       type="application/javascript"
     ></script>
     <link rel="stylesheet" href="https://unpkg.com/graphiql/graphiql.min.css" />
+    <!-- 
+      These are imports for the GraphIQL Explorer plugin.
+     -->
     <script
       src="https://unpkg.com/@graphiql/plugin-explorer/dist/index.umd.js"
       crossorigin
     ></script>
+
     <link
       rel="stylesheet"
       href="https://unpkg.com/@graphiql/plugin-explorer/dist/style.css"
     />
-    <script src="https://unpkg.com/graphql-ws@5.11.0/umd/graphql-ws.min.js"></script>
   </head>
 
   <body>
-    <div id="graphiql">
-      <div class="spinner">Loading GraphiQL...</div>
-    </div>
-
+    <div id="graphiql">Loading...</div>
     <script>
       const root = ReactDOM.createRoot(document.getElementById('graphiql'));
 
